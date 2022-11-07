b97884f6ff6192d70c93a177fe9bdf6b6adcdc8c
Daniel Andoni <dandoni@ena.com>
2022-11-07 10:08:28 +0200

    Added pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,50 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    VitePWA({
+      mode: "development",
+      base: "/",
+      srcDir: "src",
+      filename: "sw.ts",
+      includeAssets: ["/favicon.png"],
+      strategies: "injectManifest",
+      manifest: {
+        name: "Test Project",
+        short_name: "Test",
+        theme_color: "#9b59b6",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            "src": "./img/icons/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "./img/icons/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "./img/icons/android-chrome-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "./img/icons/android-chrome-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+      },
+    }),
+  ]
 })
